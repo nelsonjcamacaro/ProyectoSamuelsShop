0f9042fc2bcb2863e47f0aa95f05ee1392c2caa7
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2021-06-29 10:25:37 -0300

    Actualizacion, creacion de objetos de productos en Javascript

---
+++ logic.js
@@ -0,0 +1,11 @@
+let ProductList = [
+    {id:'0159D', name: 'Mariel',sizes: ['36','37','38','39/40'], colors: ['Taupe','blanco','azul perlado','gris claro', 'negro', 'rose'], price: 12.0},
+    {id:'0164D', name: 'Julia',sizes: ['35/36','37/38','39/40'], colors: ['Verde aguacate','azul perlado','dorado', 'negro', 'pardo','rose'], price: 8.0},
+    {id:'0187D', name: 'Positive',sizes: ['35/36','37/38','39/40'], colors: ['Azul acido','Lila pastel','Rosado pastel', 'rose', 'verde claro'], price: 5.40},
+    {id:'0168D', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90},
+    {id:'0150D', name: 'Aurora',sizes: ['35/36','37/38','39/40'], colors: ['fresa','azul perlado','amarillo','fucsia', 'mora', 'negro', 'verde claro'], price: 5.10},
+    {id:'0144D', name: 'Salome',sizes: ['35/36','37/38','39/40'], colors: ['blanco','azul perlado','beige', 'coca cola','negro', 'rose'], price: 6.20},
+    {id:'', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90},
+    {id:'', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90},
+    {id:'', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90},
+    {id:'', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90}]
