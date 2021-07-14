7850ed67ac9431e328d75f5c7f02eb2091eb15a3
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2021-07-13 22:11:36 -0300

    funcionalidad del carrito de compras completa

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 var productList = [
-    {id:'0159D', name: 'Mariel',sizes: ['36','37','38','39/40'], colors: ['Taupe','blanco','azul perlado','gris claro', 'negro', 'rose'], price: 12.0},
+    {id:'0159D', name: 'Mariel',sizes: ['36','37','38','39/40'], colors: ['Taupe','blanco','azul perlado','gris claro', 'negro', 'rose'], price: 12.0,},
     {id:'0164D', name: 'Julia',sizes: ['35/36','37/38','39/40'], colors: ['Verde aguacate','azul perlado','dorado', 'negro', 'pardo','rose'], price: 8.0},
     {id:'0187D', name: 'Positive',sizes: ['35/36','37/38','39/40'], colors: ['Azul acido','Lila pastel','Rosado pastel', 'rose', 'verde claro'], price: 5.40},
     {id:'0168D', name: 'Celia',sizes: ['35/36','37/38','39/40'], colors: ['Amarillo','blanco','azul perlado','verde claro', 'negro', 'rose'], price: 5.90},
